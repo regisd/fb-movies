fc706380f8d0fcd67a19fa87197a6a89aea2793d
Régis Décamps <decamps@users.sf.net>
2013-04-30 19:32:43 +0200

    Film & rating model

---
+++ model.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+__author__ = "Régis Décamps"
+from datetime import datetime
+
+
+class Film(object):
+    def __init__(self, title, url):
+        self.title = title
+        self.url = url
+        self.time_length = 0
+
+    def __str__(self):
+        return self.title
+
+
+class Rating(object):
+    def __init__(self, film, rating, scale=10):
+        self.film = film
+        self.score = float(rating)
+        self.scale = scale
+        self.created_time = datetime.now()
+
+    @property
+    def normalized_rating(self):
+        return self.score / self.scale
+
+    def __str__(self):
+        return '{film} ({score}/{scale})'.format(film=self.film, score=self.score, scale=self.scale, norm_rating=self.normalized_rating)
